4a4cf45518b7fc7298185fcbfc7c53ece4785fc4
Fiurmi <Farhanaurmi760@gmail.com>
2023-07-19 15:48:00 +0600

    add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import {Routes} from '@angular/router';
+import {RouterModule} from '@angular/router';
+import { PostComponent } from './post/post.component';
+import { HomeComponent } from './home/home.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: '', component: HomeComponent },
+  { path: 'create-post', component: CreatePostComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    PostComponent,
+    HomeComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
